c00ddc72201c40738b68ea4f0b386960dacbef87
Jethro Williams <jethrowilliams4@hotmail.com>
2018-12-21 11:33:43 +0000

    Return an array of the used languages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,20 @@
+require 'json'
+require 'open-uri'
+
 class PagesController < ApplicationController
   def home
   end
 
   def result
+    url = "https://api.github.com/users/#{params[:username]}/repos"
+    projects_serialized = open(url).read
+    @projects = JSON.parse(projects_serialized)
+
+    language_array = []
+    @projects.each do |project|
+      language_array << project["language"]
+    end
+
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
+
   get 'pages/home', as: :home
   get 'pages/result', as: :result
 end
