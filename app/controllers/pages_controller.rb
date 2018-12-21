eb2f10fc1f9ee4e3537cb2f335419b644ee79fc5
Jethro Williams <jethrowilliams4@hotmail.com>
2018-12-21 12:08:39 +0000

    Find-out which the user's favourite language is

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,12 +9,29 @@ class PagesController < ApplicationController
     url = "https://api.github.com/users/#{params[:username]}/repos"
     projects_serialized = open(url).read
     @projects = JSON.parse(projects_serialized)
+    get_favourite_language
+  end
 
+  def get_favourite_language
     language_array = []
     @projects.each do |project|
       language_array << project["language"]
     end
 
+    language_array.compact! # Remove instances of nil
+    languages = language_array.uniq
 
+    count = []
+    arr_index = 0
+    languages.each do |language|
+      count[arr_index] = language_array.count(language)
+      arr_index += 1
+    end
+
+    arr_index = 0
+    count.each do |num|
+      @favourite_language = languages[arr_index] if num == count.max
+      arr_index += 1
+    end
   end
 end
